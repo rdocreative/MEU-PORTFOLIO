475b71adbee74d62fb4713b02f0934b3726bf2a4
[dyad] <playgrooz368@gmail.com>
2026-01-06 14:55:59 -0300

    Reverted all changes back to version 36e3945677c9bea8fb4360a7d6f51583c9ab084f

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ToastProvider from './components/ToastProvider';
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./globals.css";
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider />
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
